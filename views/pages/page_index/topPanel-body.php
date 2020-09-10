49e843c6de09498a7c7f4c991bf895c510d20786
christophegourmand <christophe.gourmand@gmail.com>
2020-09-10 21:48:22 +0200

    [Page_photos / page-content / .gallery]  added, in progress

---
+++ views/pages/page_index/topPanel-body.php
@@ -0,0 +1,6 @@
+<div class="catchphrase">
+    <blockquote class="catchphrase-phrase">ALINEA<br>‟ En s'écartant de la ligne ″</blockquote>
+</div>
+
+<div class="phrases_sequence" id="phrases_sequence_index">
+</div>
