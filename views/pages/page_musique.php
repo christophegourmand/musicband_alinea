40fb7e931d9f38fc1fc62240bd4e7f0258048326
christophegourmand <christophe.gourmand@gmail.com>
2020-09-11 21:19:38 +0200

    2020.09.11_21h18

---
+++ views/pages/page_musique.php
@@ -0,0 +1,34 @@
+<?php 
+// VARIABLES USED TO BUILD PATH-TO-FILES :
+    $prefix_to_root_folder = "../../";
+    $page_name = "page_musique"; // used to build paths-to-files
+
+
+// VARIABLES USED IN HEADER :
+    $page_title = "ALINEA musique - Musique";
+    $page_description = "Morceaux, paroles, et plus";
+
+// VARIABLES TO ADJUST "common elements" (header, navbar, ...)
+    $active_menu_in_navbar = "Musique";
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <?php include($prefix_to_root_folder."views/common/head.php"); ?>
+
+    <body>
+        <div class="topPanel">
+            <?php include($prefix_to_root_folder."views/common/header.php"); ?>
+
+            <div class="topPanel-body">
+                <?php include($prefix_to_root_folder."views/pages/".$page_name."/topPanel-body.php"); ?>
+            </div>
+        </div>
+
+        <?php include($prefix_to_root_folder."views/pages/".$page_name."/page-content.php"); ?>
+        
+        <?php include($prefix_to_root_folder."views/common/footer.php"); ?>
+        <?php require_once($prefix_to_root_folder."views/common/load_js_scripts.php"); ?>
+    </body>
+</html>
---
+++ views/pages/page_musique/page-content.php
@@ -0,0 +1,37 @@
+<main>
+    <h1 class="page-title">Musiques</h1>
+
+    <div class="horizontalCard-container">
+        <div class="horizontalCard" id="horizontalCard_madison">
+            <h3 class="horizontalCard-title">Madison</h3>
+        </div>
+        <div class="horizontalCard" id="horizontalCard_madison">
+            <h3 class="horizontalCard-title">Soif</h3>
+        </div>
+        <div class="horizontalCard" id="horizontalCard_madison">
+            <h3 class="horizontalCard-title">Le Placard</h3>
+        </div>
+        <div class="horizontalCard" id="horizontalCard_madison">
+            <h3 class="horizontalCard-title">L'Exil</h3>
+        </div>
+        <div class="horizontalCard" id="horizontalCard_madison">
+            <h3 class="horizontalCard-title">Pas d'Autre Monde</h3>
+        </div>
+        <div class="horizontalCard" id="horizontalCard_madison">
+            <h3 class="horizontalCard-title">Post-It</h3>
+        </div>
+        <div class="horizontalCard" id="horizontalCard_madison">
+            <h3 class="horizontalCard-title">Si tu vas bien</h3>
+        </div>
+        <div class="horizontalCard" id="horizontalCard_madison">
+            <h3 class="horizontalCard-title">Blanc et Noir</h3>
+        </div>
+        <div class="horizontalCard" id="horizontalCard_madison">
+            <h3 class="horizontalCard-title">Des foules de toi</h3>
+        </div>
+        <div class="horizontalCard" id="horizontalCard_madison">
+            <h3 class="horizontalCard-title">J'ai pas les mots</h3>
+        </div>
+    </div>
+    
+</main>
