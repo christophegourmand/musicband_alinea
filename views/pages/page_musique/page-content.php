e0e42881603f28252a17e8ef0caaa6f73f05e2b7
christophegourmand <christophe.gourmand@gmail.com>
2020-09-12 21:12:49 +0200

    working on lyrics design and array

--- views/pages/page_musique/page-content.php
+++ views/pages/page_musique/page-content.php
@@ -1,37 +1,71 @@
 <main>
-    <h1 class="page-title">Musiques</h1>
+    <h2 class="page-title">Musique</h2>
+    <section class="songs split_30_70">
+        <aside class="songs-list horizontalCard-container">
 
-    <div class="horizontalCard-container">
-        <div class="horizontalCard" id="horizontalCard_madison">
-            <h3 class="horizontalCard-title">Madison</h3>
+            <?php // LOOP SUR LE TABLEAU DES CHANSONS ?>
+                <div class="horizontalCard" id="horizontalCard_madison">
+                    <h3 class="horizontalCard-title">LOOPING_ELEMENT</h3>
+                </div>
+            <?php // FIN DE LOOP ?>
+
+            <div class="horizontalCard" id="horizontalCard_madison">
+                <h3 class="horizontalCard-title">Madison</h3>
+            </div>
+            <div class="horizontalCard" id="horizontalCard_madison">
+                <h3 class="horizontalCard-title">Soif</h3>
+            </div>
+            <div class="horizontalCard" id="horizontalCard_madison">
+                <h3 class="horizontalCard-title">Le Placard</h3>
+            </div>
+            <div class="horizontalCard" id="horizontalCard_madison">
+                <h3 class="horizontalCard-title">L'Exil</h3>
+            </div>
+            <div class="horizontalCard" id="horizontalCard_madison">
+                <h3 class="horizontalCard-title">Pas d'Autre Monde</h3>
+            </div>
+            <div class="horizontalCard" id="horizontalCard_madison">
+                <h3 class="horizontalCard-title">Post-It</h3>
+            </div>
+            <div class="horizontalCard" id="horizontalCard_madison">
+                <h3 class="horizontalCard-title">Si tu vas bien</h3>
+            </div>
+            <div class="horizontalCard" id="horizontalCard_madison">
+                <h3 class="horizontalCard-title">Blanc et Noir</h3>
+            </div>
+            <div class="horizontalCard" id="horizontalCard_madison">
+                <h3 class="horizontalCard-title">Des foules de toi</h3>
+            </div>
+            <div class="horizontalCard" id="horizontalCard_madison">
+                <h3 class="horizontalCard-title">J'ai pas les mots</h3>
+            </div>
+        </aside>
+        <div class="lyric">
+            <h3 class="lyric-title">Titre du morceau</h3>
+            <article class="lyric-list">
+                <p class="lyric-paragraph">
+                    <span class="lyric-sentence">Not by way of an apology</span>
+                    <span class="lyric-sentence">For the things that I have done</span>
+                    <span class="lyric-sentence">Do I set my boat upon the sea</span>
+                </p>
+                <p class="lyric-paragraph">
+                    <span class="lyric-sentence">So like thunder I am breaking free</span>
+                    <span class="lyric-sentence">In the landscape of the heart</span>
+                    <span class="lyric-sentence">It's hard to tell what's really taken me</span>
+                </p>
+                <p class="lyric-paragraph chorus">
+                    <span class="lyric-sentence">Now I see it all through different eyes</span>
+                    <span class="lyric-sentence">This emotion can't be wrong</span>
+                    <span class="lyric-sentence">Past the mountains under empy skies</span>
+                    <span class="lyric-sentence">And the road goes on and on...</span>
+                </p>
+                <p class="lyric-paragraph">
+                    <span class="lyric-sentence">I've been living through this poetry</span>
+                    <span class="lyric-sentence">Tangled words and worn out prose</span>
+                    <span class="lyric-sentence">Love is needing, love is bleeding me</span>
+                </p>
+            </article>
         </div>
-        <div class="horizontalCard" id="horizontalCard_madison">
-            <h3 class="horizontalCard-title">Soif</h3>
-        </div>
-        <div class="horizontalCard" id="horizontalCard_madison">
-            <h3 class="horizontalCard-title">Le Placard</h3>
-        </div>
-        <div class="horizontalCard" id="horizontalCard_madison">
-            <h3 class="horizontalCard-title">L'Exil</h3>
-        </div>
-        <div class="horizontalCard" id="horizontalCard_madison">
-            <h3 class="horizontalCard-title">Pas d'Autre Monde</h3>
-        </div>
-        <div class="horizontalCard" id="horizontalCard_madison">
-            <h3 class="horizontalCard-title">Post-It</h3>
-        </div>
-        <div class="horizontalCard" id="horizontalCard_madison">
-            <h3 class="horizontalCard-title">Si tu vas bien</h3>
-        </div>
-        <div class="horizontalCard" id="horizontalCard_madison">
-            <h3 class="horizontalCard-title">Blanc et Noir</h3>
-        </div>
-        <div class="horizontalCard" id="horizontalCard_madison">
-            <h3 class="horizontalCard-title">Des foules de toi</h3>
-        </div>
-        <div class="horizontalCard" id="horizontalCard_madison">
-            <h3 class="horizontalCard-title">J'ai pas les mots</h3>
-        </div>
-    </div>
+    </section>
     
 </main>
