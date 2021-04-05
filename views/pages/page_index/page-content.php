dba99a6af3d3fc4355eb552c48766a719d21fdd2
christophegourmand <christophe.gourmand@gmail.com>
2021-04-05 20:24:06 +0200

    modify /page_index/page-content.php : grammar corrections

--- views/pages/page_index/page-content.php
+++ views/pages/page_index/page-content.php
@@ -1,7 +1,8 @@
 <main>
     <section class="bigquote">
-        <blockquote class="bigquote-text">Etrange.<br>Comme tout l’est.<br>Comme Nous sommes.<br>Toujours à deux doigts de vivre vraiment… Avec vous. Avec Nous. Avec sois-même.<br>Nous sommes nos chairs inconnues, nous sommes  « l’autrement », l’exception aux règles, ce que le bruit de vivre est au silence de nos solitudes, ce que la destination est au chemin, quand l’arrivée se dessine enfin, au loin.<br>A la fois le commencement, et à la fois la fin d’une nouvelle idée.<br>L’Alinéa à tout ça…</blockquote>
+        <blockquote class="bigquote-text">Étrange.<br>Comme tout l’est.<br>Comme Nous sommes.<br>Toujours à deux doigts de vivre vraiment… Avec vous. Avec Nous. Avec soi-même.<br>Nous sommes nos chairs inconnues, nous sommes  « l’autrement », l’exception aux règles, ce que le bruit de vivre est au silence de nos solitudes, ce que la destination est au chemin, quand l’arrivée se dessine enfin, au loin.<br>À la fois le commencement, et à la fois la fin d’une nouvelle idée.<br>L’Alinéa a tout ça…</blockquote>
         <cite class="bigquote-author"></cite>
+        <!-- CORRECTIONS: "Etrange" : É,  "sois-même" : soi-même,  "A la fois" : À  ,  "L'Alinéa à tout ça" : a -->
     </section>
 
     <?php include($prefix_to_root_folder."views/modules/socialmedia.php")?>
