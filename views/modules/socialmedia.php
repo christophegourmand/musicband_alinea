8d5be7ecc7e9344ec539ccb2e094576cb5b27301
christophegourmand <christophe.gourmand@gmail.com>
2021-04-06 20:12:08 +0200

    modify alt and mouse-hover text for social-network logos

--- views/modules/socialmedia.php
+++ views/modules/socialmedia.php
@@ -2,13 +2,13 @@
     <p class="socialMedia-slogan">N'hésitez pas à venir nous suivre sur les réseaux Sociaux !</p>
     <div class="socialMedia-link-container">
         <a class="socialMedia-link bg-facebook" href="https://www.facebook.com/Alineamusique" target="_blank">
-            <img class="socialMedia-logo" src="<?php echo($prefix_to_root_folder.'/assets/img/partners/facebook/f_logo_RGB-White_144.png');?>" alt="logo plateforme musicale" title="logo plateforme musicale">
+            <img class="socialMedia-logo" src="<?php echo($prefix_to_root_folder.'/assets/img/partners/facebook/f_logo_RGB-White_144.png');?>" alt="logo facebook" title="logo facebook">
         </a>
         <a class="socialMedia-link bg-youtube" href="https://www.youtube.com/channel/UCKoysXy-RHdX1QtP4Ugr9Fw" target="_blank">
-            <img class="socialMedia-logo" src="<?php echo($prefix_to_root_folder.'/assets/img/partners/youtube/yt_icon_mono_dark.png');?>" alt="logo plateforme musicale" title="logo plateforme musicale">
+            <img class="socialMedia-logo" src="<?php echo($prefix_to_root_folder.'/assets/img/partners/youtube/yt_icon_mono_dark.png');?>" alt="logo youtube" title="logo youtube">
         </a>
         <a class="socialMedia-link bg-instagram" href="https://www.instagram.com/alineamusique/" target="_blank">
-            <img class="socialMedia-logo" src="<?php echo($prefix_to_root_folder.'/assets/img/partners/instagram/glyph-logo_May2016_modified_white_400px.png');?>" alt="logo plateforme musicale" title="logo plateforme musicale">
+            <img class="socialMedia-logo" src="<?php echo($prefix_to_root_folder.'/assets/img/partners/instagram/glyph-logo_May2016_modified_white_400px.png');?>" alt="logo instagram" title="logo instagram">
         </a>
     </div>
 </section>
