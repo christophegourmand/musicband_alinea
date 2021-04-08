9b8328cbfa50a79991a2f5794ae0cb04ab8e9076
christophegourmand <christophe.gourmand@gmail.com>
2021-04-08 21:19:06 +0200

    add background red on hover in page Contact

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -7,3 +7,19 @@
     menuToggler_node.addEventListener("click", function(){
         menusToShow_node.classList.toggle("show");    
     });
+
+
+// ANIMATION ON PAGE 'CONTACT'
+
+var listOfSectionsContact_arr = Array.from( document.querySelectorAll(".contact") );
+
+console.log(listOfSectionsContact_arr[1]);
+/* 
+    next step: 
+        1- detecter à quel moment une section (par ex: Coppa Studio), est au milieu de l'écran.
+        2- ajouter des events listener
+        3- faire que le background devienne darkred quand c'est le cas.
+
+        = ainsi, ça fonctionnera aussi sur Mobile. 
+        (car pour l'instant j'ai mis un :hover qui fonctionne seulement sur pc).
+*/
