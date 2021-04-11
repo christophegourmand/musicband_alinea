f9cf40fcd64ebf9a3e94c18f253baa3f04fd63e1
christophegourmand <christophe.gourmand@gmail.com>
2021-04-12 00:59:05 +0200

    created function animateElementOnAppear() , next step is to use it

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -77,15 +77,62 @@ console.log(listOfSectionsContact_arr[1]);
     // OK J'ARRIVE À SAVOIR QUELLE EST MA POSITION DE SCROLL DANS LA PAGE.
     // MAINTENANT JE DOIS RÉUSSIR À CONNAÎTRE LA POSITION DE L'ÉLÉMENT AVANT, ET CELUI APRÈS, L'ÉLÉMENT QUE JE VEUX ANIMER
 
-// J'ESSAYE DE DETECTER LA POSITION DES ELEMENTS DANS LA PAGE:
-    divSocialMedia_node = document.getElementById('socialMedia_id');
 
-    console.log(divSocialMedia_node.offsetHeight); // en largeur LG :  environ 378px
-    console.log(divSocialMedia_node.offsetTop); // en largeur LG :  environ 1275px  JE PENSE QUE C'EST CETTE VALEUR QUE JE VEUX !
-    console.log(divSocialMedia_node.scrollHeight); // en largeur LG :  environ 378px
-    
-    // CONCLUSION : offsetHeight et scrollHeight indiquent TOUJOURS la même valeur !  (
+function animateElementOnAppear(element_node, cssClassToAdd_str){
+    var elementPositionTop = element_node.offsetTop;
+    var elementHeight = element_node.offsetHeight;
+    var elementPositionBottom = elementPositionTop + elementHeight;
 
-    // VOIR NOTION POUR CONTINUER : https://www.notion.so/j-essaye-de-faire-se-d-clencher-des-l-ments-lors-du-scroll-b682364901304fb8abd2da4ad4d719ba
-    // et VOIR CETTE PAGE : https://stackoverflow.com/questions/487073/how-to-check-if-element-is-visible-after-scrolling 
+    window.addEventListener('scroll', function(event){
+        scrollPositionY = window.scrollY;
+        if (scrollPositionY > elementPositionBottom ) {
+            element_node.classList.add(cssClassToAdd_str);
+        }
+    });
+}
+
+
+//* ANIMATION ON PAGE 'HOME' ========================================================
+    window.addEventListener('scroll', function(event){
+        
+        // Position de la div social_media depuis le haut:
+            divSocialMedia_node = document.getElementById('socialMedia_id');
+            positionDivSocialMedia_nbr = divSocialMedia_node.offsetTop; // en largeur LG :  environ 1275px
+                /* cette valeur signifie : pour que la div socialMedia se trouve en haut de la fenêtre*/
+            //console.debug(`positionDivSocialMedia_nbr: ${positionDivSocialMedia_nbr}` );
     
+        
+            // position actuelle du scroll dans la page:
+            scrollPositionY = window.scrollY;
+            console.debug(`window.scrollY: ${window.scrollY}` );
+            
+            // detecter si on a scrollé au point d'afficher la divSocialMedia :
+            /*
+            - à 676px de scroll: debut d'apparition de la div socialMedia
+            - a 947 de scroll: les 3 icones sont finies d'être affichées.
+            */
+
+            var socialMediaIcons = Array.from(document.getElementsByClassName("socialMedia-link"));
+                                                                            // console.log("socialMediaIcons");
+                                                                            // console.log(socialMediaIcons);
+
+            if (scrollPositionY > 1020 ) {
+                                                                            console.info("PLAY ANIMATION");
+                
+                // var delayBeforeAppear = 300;
+                                                            console.debug(socialMediaIcons);
+                socialMediaIcons.forEach( function(element) {
+                    //element.style.transitionDelay = delayBeforeAppear + 150;
+                                                                            // console.debug(delayBeforeAppear);
+                    element.classList.add("play-animation");
+                });
+
+                
+            }
+    });    
+
+
+        // VOIR NOTION POUR CONTINUER : https://www.notion.so/j-essaye-de-faire-se-d-clencher-des-l-ments-lors-du-scroll-b682364901304fb8abd2da4ad4d719ba
+        // et VOIR CETTE PAGE : https://stackoverflow.com/questions/487073/how-to-check-if-element-is-visible-after-scrolling 
+        
+        
--- views/modules/socialmedia.php
+++ views/modules/socialmedia.php
@@ -1,13 +1,13 @@
 <section id="socialMedia_id" class="socialMedia">
     <p class="socialMedia-slogan">N'hésitez pas à venir nous suivre sur les réseaux Sociaux !</p>
     <div class="socialMedia-link-container">
-        <a class="socialMedia-link bg-facebook" href="https://www.facebook.com/Alineamusique" target="_blank">
+        <a class="socialMedia-link bg-facebook animation-appearsFromBottom" href="https://www.facebook.com/Alineamusique" target="_blank">
             <img class="socialMedia-logo" src="<?php echo($prefix_to_root_folder.'/assets/img/partners/facebook/f_logo_RGB-White_144.png');?>" alt="logo facebook" title="logo facebook">
         </a>
-        <a class="socialMedia-link bg-youtube" href="https://www.youtube.com/channel/UCKoysXy-RHdX1QtP4Ugr9Fw" target="_blank">
+        <a class="socialMedia-link bg-youtube animation-appearsFromBottom" href="https://www.youtube.com/channel/UCKoysXy-RHdX1QtP4Ugr9Fw" target="_blank">
             <img class="socialMedia-logo" src="<?php echo($prefix_to_root_folder.'/assets/img/partners/youtube/yt_icon_mono_dark.png');?>" alt="logo youtube" title="logo youtube">
         </a>
-        <a class="socialMedia-link bg-instagram" href="https://www.instagram.com/alineamusique/" target="_blank">
+        <a class="socialMedia-link bg-instagram animation-appearsFromBottom" href="https://www.instagram.com/alineamusique/" target="_blank">
             <img class="socialMedia-logo" src="<?php echo($prefix_to_root_folder.'/assets/img/partners/instagram/glyph-logo_May2016_modified_white_400px.png');?>" alt="logo instagram" title="logo instagram">
         </a>
     </div>
