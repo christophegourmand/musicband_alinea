daaaadfef6e70e0fb0d3d8428e644f4b8a6da6fb
christophegourmand <christophe.gourmand@gmail.com>
2021-10-22 12:39:56 +0200

    remove concert 11 September 2021 from page Tour

--- views/pages/page_tour/page-content.php
+++ views/pages/page_tour/page-content.php
@@ -5,15 +5,6 @@
     <section class="tour">
         <h3 class="tour-title">Concerts</h3>
         <div class="tour-body">
-
-            <div class="tour-event">
-                <time class="tour-event-date">11 Septembre 2021</time>
-                <p class="tour-event-location">
-                    Concert privé sur invitation:<br>double live, en collaboration avec le groupe "Rocking Chairs"
-                    <!-- <a class="tour-event-mapIcon" href="https://goo.gl/maps/m6fNwUnZwG9DxDAaA" target="_blank"><i class="fas fa-map"></i></a> -->
-                </p>
-                <p class="tour-event-city">Beynost (Ain)</p>
-            </div>
             
             <div class="tour-event">
                 <time class="tour-event-date">06 Novembre 2021 - à 20h</time>
