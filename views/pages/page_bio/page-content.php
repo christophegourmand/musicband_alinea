6dc575fe2c361443c259cb6803a9f3585d7b906b
christophegourmand <christophe.gourmand@gmail.com>
2021-10-22 12:37:05 +0200

    MODIFIY page Bio to add space between first and last name

--- views/pages/page_bio/page-content.php
+++ views/pages/page_bio/page-content.php
@@ -13,7 +13,7 @@
             </div>
         </div>
         <div class="musiciens-container">
-            <h3 class="musiciens-name">Thierry<span class="musiciens-name-lastname">Cividino</span></h3>
+            <h3 class="musiciens-name">Thierry <span class="musiciens-name-lastname">Cividino</span></h3>
             <div class="musiciens-2col">
                 <!-- <div class="musiciens-bgphoto img-thierry"></div> -->
                 <img class="musiciens-img" src="<?php echo($prefix_to_root_folder.'/assets/img/photos/musicians/photo_presentation_thierry2.jpg');?>" alt="photo du musicien" title="photo du musicien">
@@ -24,7 +24,7 @@
         </div>
 
         <div class="musiciens-container">
-            <h3 class="musiciens-name">Nico<span class="musiciens-name-lastname">Cividino</span></h3>
+            <h3 class="musiciens-name">Nico <span class="musiciens-name-lastname">Cividino</span></h3>
             <div class="musiciens-2col">
                 <!-- <div class="musiciens-bgphoto img-nico"></div> -->
                 <img class="musiciens-img" src="<?php echo($prefix_to_root_folder.'/assets/img/photos/musicians/photo_presentation_nico.jpg');?>" alt="photo du musicien" title="photo du musicien">
@@ -37,7 +37,7 @@
         </div>
 
         <div class="musiciens-container">
-            <h3 class="musiciens-name">Romain<span class="musiciens-name-lastname">Hugon</span></h3>
+            <h3 class="musiciens-name">Romain <span class="musiciens-name-lastname">Hugon</span></h3>
             <div class="musiciens-2col">
                 <!-- <div class="musiciens-bgphoto img-romain"></div> -->
                 <img class="musiciens-img" src="<?php echo($prefix_to_root_folder.'/assets/img/photos/musicians/photo_presentation_romain.jpg');?>" alt="photo du musicien" title="photo du musicien">
@@ -47,7 +47,7 @@
         </div>
         
         <div class="musiciens-container">
-            <h3 class="musiciens-name">Nicky<span class="musiciens-name-lastname">Rafidinanahary</span></h3>
+            <h3 class="musiciens-name">Nicky <span class="musiciens-name-lastname">Rafidinanahary</span></h3>
             <div class="musiciens-2col">
                 <!-- <div class="musiciens-bgphoto img-nicole"></div> -->
                 <img class="musiciens-img" src="<?php echo($prefix_to_root_folder.'/assets/img/photos/musicians/photo_presentation_nicole.jpg');?>" alt="photo du musicien" title="photo du musicien">
@@ -57,7 +57,7 @@
         </div>
 
         <div class="musiciens-container">
-            <h3 class="musiciens-name">Anthony<span class="musiciens-name-lastname">Plaisir</span></h3>
+            <h3 class="musiciens-name">Anthony <span class="musiciens-name-lastname">Plaisir</span></h3>
             <div class="musiciens-2col">
                 <img class="musiciens-img" src="<?php echo($prefix_to_root_folder.'/assets/img/photos/musicians/photo_presentation_anthony.jpg');?>" alt="photo du musicien" title="photo du musicien">
                 <p class="musiciens-description">Originaire d’Angers, Guitariste électro-acoustique depuis sa plus tendre enfance, ses arpèges et rythmiques font de lui le compagnon idéal d’intégration dans cette belle aventure.<br>
