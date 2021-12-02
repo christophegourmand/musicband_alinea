a0dc3430359a409b22e5c80b07b0fed5fa00ae85
christophegourmand <christophe.gourmand@gmail.com>
2021-12-02 21:26:06 +0100

    feat(tour): add concert 9 april 2022

--- views/pages/page_tour/page-content.php
+++ views/pages/page_tour/page-content.php
@@ -5,16 +5,15 @@
     <section class="tour">
         <h3 class="tour-title">Concerts</h3>
         <div class="tour-body">
-            <!--
+            
             <div class="tour-event">
-                <time class="tour-event-date">06 Novembre 2021 - à 20h</time>
+                <time class="tour-event-date">09 Avril 2022 - à 21h</time>
                 <p class="tour-event-location">
-                    L'Éveil Des Puys
-                    <a class="tour-event-mapIcon" href="https://g.page/EdP63?share" target="_blank"><i class="fas fa-map"></i></a>
+                    brasserie Les Ursulines
+                    <a class="tour-event-mapIcon" href="https://goo.gl/maps/dfiKFMc7ahCrUYkUA" target="_blank"><i class="fas fa-map"></i></a>
                 </p>
-                <p class="tour-event-city">Ceyssat (Puis De Dôme)</p>
+                <p class="tour-event-city">Crémieux</p>
             </div>
-            -->
 
         </div>
     </section>
